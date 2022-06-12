b56e4e47b0ed5f32dbdc34e80c6df33e57ed3f38
Владислав <utkinvaa@gmail.com>
2022-06-12 13:02:10 +0300

    commit

--- BeautySaloonProj/Properties/Resources.Designer.cs
+++ BeautySaloonProj/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace BeautySaloonProj.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap favlogo {
-            get {
-                object obj = ResourceManager.GetObject("favlogo", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
